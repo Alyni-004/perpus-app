19f97bdc010916562360e84ad2df375fbe29f520
Alyni-004 <alyni6605@gmail.com>
2026-09-23 12:20:32 +0700

    Pertemuan 3 - Routing, Controller, dan Blade dasar

---
+++ resources/views/kontak.blade.php
@@ -0,0 +1,8 @@
+@extends('layouts.app')
+
+@section('title', 'Kontak')
+
+@section('content')
+    <h1>Halaman Kontak</h1>
+    <p>Ini adalah halaman kontak Sistem Informasi Peminjaman Buku Perpustakaan Mini.</p>
+@endsection
---
+++ resources/views/tentang.blade.php
@@ -0,0 +1,8 @@
+@extends('layouts.app')
+
+@section('title', 'Tentang Aplikasi')
+
+@section('content')
+    <h1>Tentang Aplikasi</h1>
+    <p>Aplikasi ini dibuat sebagai studi kasus pembelajaran mata kuliah Pemrograman Web II.</p>
+@endsection
